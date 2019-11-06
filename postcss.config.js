a1abe3e249bffbdbde88f0ff543a10c8260d4ba7
Collin <52185661+scs727c@users.noreply.github.com>
2019-11-05 19:37:40 -0600

    Vue commit
    Missing landing page, but oh well

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,13 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [
-        '*.html',
-    ],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-})
-
-const cssnano = require('cssnano')({
-            preset: 'default',
-        })
-
 module.exports = {
-    plugins: [
-        require('postcss-import'),
-        require('tailwindcss'),
-        require('autoprefixer'),
-        ...process.env.NODE_ENV === 'production'
-            ? [purgecss, cssnano]
-            : []
-    ]
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+      content: [
+        './src/**/*.vue',
+        './public/index.html',
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  ]
 }
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="home" class="antialiased text-gray-900">
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'home',
+  components: {
+
+  }
+}
+</script>
