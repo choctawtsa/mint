b24dcbcef966b87c9f9f8cf507884366d36d518e
Collin <vicepresident@choctawtsa.org>
2019-11-07 11:06:10 -0600

    Rearranged navbar
    might be broken

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,11 +2,6 @@
   <header id="menu" class="fixed w-screen bg-white shadow-md top-0 left-0">
     <div class="container mx-auto sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">
       <div class="flex items-center justify-between px-4 py-3 sm:p-0">
-        <div>
-          <router-link to="/">
-            <img class="h-8" src="/img/logos/light-background.svg" alt="Mint">
-          </router-link>
-        </div>
         <div class="sm:hidden">
           <button @click="isOpen = !isOpen" type="button" class="block text-gray-700 hover:text-gray-500 focus:text-gray-500 focus:outline-none">
             <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
@@ -14,6 +9,11 @@
               <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
             </svg>
           </button>
+        </div>
+        <div>
+          <router-link to="/">
+            <img class="h-8" src="/img/logos/light-background.svg" alt="Mint">
+          </router-link>
         </div>
       </div>
       <nav :class="isOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4 sm:flex sm:p-0">
