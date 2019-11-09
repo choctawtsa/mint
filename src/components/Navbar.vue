d575cd005c2b8ed9aca92217adf92c57c3aac4c2
Collin <vicepresident@choctawtsa.org>
2019-11-09 12:37:02 -0600

    Navbar fix Hopefully

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,8 +7,7 @@
             <img class="h-8" src="/img/logos/light-background.svg" alt="Mint">
           </router-link>
         </div>
-      </div>
-      <div class="sm:hidden">
+        <div class="sm:hidden">
           <button @click="isOpen = !isOpen" type="button" class="block text-gray-700 hover:text-gray-500 focus:text-gray-500 focus:outline-none">
             <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
               <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
@@ -16,14 +15,13 @@
             </svg>
           </button>
         </div>
-        
+      </div>
       <nav :class="isOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4 sm:flex sm:p-0">
         <router-link to="/" class="block px-2 py-1 text-gray-700 font-semibold rounded hover:bg-gray-200">Home</router-link>
         <router-link to="/dictionary" class="mt-1 block px-2 py-1 text-gray-700 font-semibold rounded hover:bg-gray-200 sm:mt-0 sm:ml-2">Dictionary</router-link>
         <a href="https://www.choctawtsa.org" class="mt-1 block px-2 py-1 text-gray-700 font-semibold rounded hover:bg-gray-200 sm:mt-0 sm:ml-2">Choctaw TSA</a>
       </nav>
     </div>
-      
   </header>
 </template>
 
