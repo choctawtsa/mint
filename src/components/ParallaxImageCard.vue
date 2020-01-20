ce9051d6a9bdde7494a4899242ad59bb08a9675d
Collin <52185661+scs727c@users.noreply.github.com>
2020-01-19 21:38:48 -0600

    Made Image in card visibile on smaller screens

--- src/components/ParallaxImageCard.vue
+++ src/components/ParallaxImageCard.vue
@@ -2,9 +2,11 @@
   <kinesis-container>
     <kinesis-element :strength="1" type="depth" tag="div">
       <div
-        class="flex w-full mx-auto overflow-hidden bg-gray-900 rounded-lg shadow-lg"
+        class="flex flex-col w-full mx-auto overflow-hidden bg-gray-900 rounded-lg shadow-lg lg:flex-row"
       >
-        <div class="flex-shrink-0 hidden w-1/3 lg:relative lg:block xl:w-1/2">
+        <div
+          class="relative flex-shrink-0 block w-full h-32 lg:w-1/3 lg:h-auto xl:w-1/2"
+        >
           <img
             class="absolute inset-0 object-cover object-center w-full h-full"
             :src="imgUrl"
@@ -12,7 +14,7 @@
           />
         </div>
         <div
-          class="w-2/3 mx-8 py-12 sm:mx-12 sm:py-16 lg:max-w-lg xl:w-1/2 2xl:mx-20 2xl:py-20"
+          class="mx-8 py-12 sm:mx-12 sm:py-16 lg:w-2/3 lg:max-w-lg xl:w-1/2 2xl:mx-20 2xl:py-20"
         >
           <kinesis-element :strength="3" type="depth" tag="div">
             <p class="font-mono font-medium text-gray-500 uppercase lg:text-lg">
